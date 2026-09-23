70c691d4c75082f46f3cf61a6087143c8ff49a38
khandelwal440 <khandelwal440@github.com>
2026-09-23 14:01:38 +0530

    Revert "Fix desktop gallery layout: center showcase framing, proper depth shadows & diverse photos"
    This reverts commit fad12c4d4e3c49da54019dd5095390a47bf47019.

--- src/components/about/GallerySection.jsx
+++ src/components/about/GallerySection.jsx
@@ -9,27 +9,27 @@ const galleryImages = [
     className: 'gallery__image-wrapper--first',
   },
   {
-    src: '/photos/DSC00615.JPG',
+    src: '/photos/DSC00580.JPG',
     alt: 'Aluma Salon premium atmosphere',
     className: 'gallery__image-wrapper--second',
   },
   {
-    src: '/photos/DSC00635.JPG',
+    src: '/photos/DSC00582.JPG',
     alt: 'Aluma Salon hair styling & grooming',
     className: 'gallery__image-wrapper--thrd',
   },
   {
-    src: '/photos/DSC00650.JPG',
+    src: '/photos/DSC00585.JPG',
     alt: 'Aluma Salon expert care',
     className: 'gallery__image-wrapper--fourth',
   },
   {
-    src: '/photos/DSC00665.JPG',
+    src: '/photos/DSC00588.JPG',
     alt: 'Aluma Salon luxury studio',
     className: 'gallery__image-wrapper--fifth',
   },
   {
-    src: '/photos/DSC00680.JPG',
+    src: '/photos/DSC00591.JPG',
     alt: 'Aluma Salon client personalization',
     className: 'gallery__image-wrapper--sixth',
   },
